bb4be6080c32602ff58f2a641e83949591c521ec
DealwithDon <107222175+DealwithDon@users.noreply.github.com>
2025-01-06 20:05:06 +0100

    Losowanie script.js

--- script.js
+++ script.js
@@ -12,3 +12,39 @@
         }, { once: true });
     });
 // });
+var x;
+var slot1 = [];
+var slot2 = [];
+var slot3 = [];
+
+function losujLiczbe() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function losujLiczbeObrotow() {
+    x = Math.floor(Math.random() * 6) + 4;
+}
+
+function dodajLiczbeDoTablicyXrazy(x, tablica) {
+    for (var i = 0; i < x; i++) {
+        let liczba = losujLiczbe();
+        tablica.push(liczba);
+    }
+}
+
+function losujOwoce() {
+    losujLiczbeObrotow();
+    dodajLiczbeDoTablicyXrazy(x, slot1);
+
+    losujLiczbeObrotow();
+    dodajLiczbeDoTablicyXrazy(x, slot2);
+
+    losujLiczbeObrotow();
+    dodajLiczbeDoTablicyXrazy(x, slot3);
+
+    console.log("Slot 1:", slot1);
+    console.log("Slot 2:", slot2);
+    console.log("Slot 3:", slot3);
+}
+
+losujOwoce();
